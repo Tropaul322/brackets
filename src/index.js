88f5d5d869a0256840140bfe73b5c72849bfd9a3
Ymbi <60569456+Tropaul322@users.noreply.github.com>
2020-03-19 16:30:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports =function check(str, bracketsConfig) {
+  var closeBr = "";
+  for (var i =0; i< str.length; i ++){
+    for (var y = 0; y<bracketsConfig.length; y++){
+      if (bracketsConfig[y].includes(str[i])){
+        if (closeBr == ""){
+          if(str[i] == bracketsConfig[y][0]){
+            closeBr += bracketsConfig[y][1];
+          } else {
+            return false
+          }
+        }else {
+          if (str[i]==closeBr.slice(-1)){
+            closeBr = closeBr.slice(0, -1); 
+          } else {
+            if (str[i]== bracketsConfig[y][0]){
+              closeBr += bracketsConfig[y][1];
+            }else{
+              return false
+            }
+          }
+        }
+      }
+    }
+  }
+  if (closeBr == ""){
+    return true
+  }else {
+    return false;
+  }
+
 }
+
